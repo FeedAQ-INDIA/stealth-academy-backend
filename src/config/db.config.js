6d7ca60056aa400445b851ab3f4bb186d6fdd6f1
Avi9631 <avikumarshooters@gmail.com>
2025-04-09 16:39:37 +0530

    added postgres for supabase connect

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -11,11 +11,12 @@ module.exports = {
         idle: 10000
     }
 
-    // HOST: "dpg-cv1ek01u0jms738blt2g-a",
-    // USER: "user",
-    // PASSWORD: "ONrednq2JHcCJhBIStH2okhpiA2D3lAZ",
-    // DB: "feedaq-xm-node",
-    // dialect: "mysql",
+    // HOST: "db.vtsiasguucuvjjqqooeo.supabase.co",
+    // USER: "postgres",
+    // PASSWORD: "Feedaq@123", // replace with your actual Supabase password
+    // DB: "postgres",
+    // dialect: "postgres",
+    // port: 5432,
     // pool: {
     //     max: 5,
     //     min: 0,
