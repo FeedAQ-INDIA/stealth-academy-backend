c7c4e9210450271c0f1ca1f8d5d08dd4dbd22589
Avi9631 <avikumarshooters@gmail.com>
2025-04-11 09:42:41 +0530

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "password",
-    DB: "feedaq-academy-node",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-
-    // HOST: "db.vtsiasguucuvjjqqooeo.supabase.co",
-    // USER: "postgres",
-    // PASSWORD: "Feedaq@123", // replace with your actual Supabase password
-    // DB: "postgres",
-    // dialect: "postgres",
-    // port: 5432,
+    // HOST: "localhost",
+    // USER: "root",
+    // PASSWORD: "password",
+    // DB: "feedaq-academy-node",
+    // dialect: "mysql",
     // pool: {
     //     max: 5,
     //     min: 0,
     //     acquire: 30000,
     //     idle: 10000
     // }
+
+    HOST: "db.vtsiasguucuvjjqqooeo.supabase.co",
+    USER: "postgres",
+    PASSWORD: "Feedaq@123", // replace with your actual Supabase password
+    DB: "postgres",
+    dialect: "postgres",
+    port: 5432,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 };
   
